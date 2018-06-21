2c7772173103e014ae37bc73b51daafd2d67ea49
vladkorkach <vladkorkach.dev@gmail.com>
2018-06-21 11:58:32 +0300

    Added dev urls to py config

--- backend/names_project/settings.py
+++ backend/names_project/settings.py
@@ -152,10 +152,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+# local
+# BOY_NAMES_URL = "http://localhost:8081/"
+# GIRL_NAMES_URL = "http://localhost:8082/"
 
-BOY_NAMES_URL = "http://localhost:8081/"
-GIRL_NAMES_URL = "http://localhost:8082/"
-
+# dev
+BOY_NAMES_URL = "http://boy_names.devhost.com/"
+GIRL_NAMES_URL = "http://girl_names.devhost.com/"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
